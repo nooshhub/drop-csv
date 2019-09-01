d7f49113ce89d4d88198ebd0c2755959b64dc2df
nealshen2 <shandeai520@gmail.com>
2019-09-01 21:27:48 +0800

    add solutions for read csv and preview it

--- csv-react/src/pages/index.js
+++ csv-react/src/pages/index.js
@@ -6,7 +6,7 @@ const { Dragger } = Upload;
 const props = {
   name: 'file',
   multiple: false,
-  accept: '.csv',
+  accept: 'application/vnd.ms-excel',
   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
   onChange(info) {
     const { status } = info.file;
@@ -19,7 +19,54 @@ const props = {
       message.error(`${info.file.name} file upload failed.`);
     }
   },
+  beforeUpload: beforeUpload
 };
+
+function beforeUpload(file) {
+  const isCsv = file.type === 'application/vnd.ms-excel';
+  if (!isCsv) {
+    message.error('You can only upload csv file!');
+  }
+  // const isLt2M = file.size / 1024 / 1024 < 2;
+  // if (!isLt2M) {
+  //   message.error('Image must smaller than 2MB!');
+  // }
+  // return isCsv && isLt2M;
+
+  // TODO:
+  // if we not limit here, and show the top 10 rows of data,
+  // how do we do that
+
+  console.log(file);
+  if (typeof (FileReader) !== 'undefined') {    //H5
+    var reader = new FileReader();
+    reader.readAsText(file);            //以文本格式读取
+    // reader.readAsArrayBuffer(file);            //以文本格式读取
+    
+    // TODO: limit the load progress at 1000 bytes
+
+    reader.onload = function (evt) {
+      var data = evt.target.result;        //读到的数据
+      var lineBreakIndex = data.indexOf("\n");
+      console.log(data.substr(0, lineBreakIndex));
+      // Get Line breaker and read line by line, until we hit the end
+      // and only show the first 4, right.
+
+      for (let i = 0; i < 3; i++) {
+        console.log(i);
+      }
+
+      console.log(data);
+      // console.log(data.byteLength);
+      // var view   = new DataView(data, 0, 200);
+      // console.log(view.byteLength);
+    }
+  } else {
+    message.error("IE9及以下浏览器不支持，请使用Chrome或Firefox浏览器");
+  }
+
+  return isCsv;
+}
 
 const dataSource = [
   {
