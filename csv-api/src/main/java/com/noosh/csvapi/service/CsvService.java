b480c7c82d39dc60f757a695797afb03ffff5545
neals <shandeai520@gmail.com>
2019-09-01 23:38:49 -0700

    csv-api save

--- csv-api/src/main/java/com/noosh/csvapi/service/CsvService.java
+++ csv-api/src/main/java/com/noosh/csvapi/service/CsvService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Neal Shan
@@ -16,15 +19,18 @@ public class CsvService {
 
     @PostConstruct
     public void init() {
-        parseExcelCsv(1);
+        File file = new File("I:\\cloud\\drop-csv\\csv-api\\csv-files\\Reinsurance.csv");
+
+        String[] headers = {"UniqueName", "Setid", "Name",
+                "cus_REINS", "cus_DESCRSHORT", "cus_REINS_DESCR_", "cus_SETID_REINS_"};
+
+        parseExcelCsv(file, headers, 1);
     }
 
-    public void parseExcelCsv(int skipLineCount) {
+    public void parseExcelCsv(File file, String[] headers, int skipLineCount) {
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader("I:\\cloud\\drop-csv\\csv-api\\csv-files\\Reinsurance.csv"));
-
-
+            in = new BufferedReader(new FileReader(file));
         } catch (FileNotFoundException e) {
             // TODO: return a file not found message
         }
@@ -41,12 +47,11 @@ public class CsvService {
         }
 
 
+        // parse
         Iterable<CSVRecord> records = null;
         try {
             if (in != null) {
-                String[] headers = {"UniqueName", "Setid", "Name",
-                        "cus_REINS", "cus_DESCRSHORT", "cus_REINS_DESCR_", "cus_SETID_REINS_"};
-                records = CSVFormat.EXCEL.withFirstRecordAsHeader().withHeader(headers).parse(in);
+                records = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
             }
         } catch (IOException e) {
             // TODO: return a file not found message
@@ -54,9 +59,12 @@ public class CsvService {
 
         if (records != null) {
             for (CSVRecord record : records) {
-                String lastName = record.get("UniqueName");
-                String firstName = record.get("Setid");
-                System.out.println(lastName + "  " + firstName);
+                Map<String, String> line = new HashMap<>(headers.length);
+                for (String header: headers) {
+                    line.put(header, record.get(header));
+                }
+                System.out.println(line);
+                // TODO: map will take too much bandwidth, use headers + same sort order data list []
             }
         }
 
