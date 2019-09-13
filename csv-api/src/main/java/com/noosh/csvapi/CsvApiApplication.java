93465273755a0272abf695b1eaa9b95a64d0c793
nealshen2 <shandeai520@gmail.com>
2019-09-13 09:45:50 +0800

    clean up

--- csv-api/src/main/java/com/noosh/csvapi/CsvApiApplication.java
+++ csv-api/src/main/java/com/noosh/csvapi/CsvApiApplication.java
@@ -1,22 +1,9 @@
 package com.noosh.csvapi;
 
-import com.noosh.csvapi.dao.CsvHeader;
-import com.noosh.csvapi.dao.Customer;
 import com.noosh.csvapi.service.StorageProperties;
-import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
