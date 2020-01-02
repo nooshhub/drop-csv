3103e615506c2921ecd0bb1835614f6b75f75274
neals <shandeai520@gmail.com>
2020-01-01 22:17:14 -0800

    netty websocket benchmark

---
+++ csvserver/src/main/java/csv/server/sample/http/websocketx/benchmarkserver/WebSocketServerInitializer.java
@@ -0,0 +1,32 @@
+package csv.server.sample.http.websocketx.benchmarkserver;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * @author Neal Shan
+ * @since 0.0.1
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public WebSocketServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (sslCtx != null) {
+            pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65536));
+        pipeline.addLast(new WebSocketServerHandler());
+    }
+}
